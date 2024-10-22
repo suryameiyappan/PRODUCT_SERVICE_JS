7824ed87f3a535bb62f83c64e6c3a23c35055f9d
suryameiyappan <137778852+suryameiyappan@users.noreply.github.com>
2024-10-22 13:06:12 +0530

    Update module.helper.js

--- modules/utils/module.helper.js
+++ modules/utils/module.helper.js
@@ -1,4 +1,6 @@
-const { loadFactory } = require("../../config/factory-config/modules.config");
+const 
+  { SUB_PRODUCT_ROUTES } = require("../../constants/modules.constant"),
+  { loadFactory } = require("../../config/factory-config/modules.config");
 /*
 |--------------------------------------------------------------------------
 | GET Module OBJECT
@@ -8,7 +10,7 @@ exports.getModuleProductObject = async (request) => {
   const moduleCode = request.body.module_code;
   const action = request.body.action;
   const module = request.body.module;
-  const moduleClass = await loadFactory(moduleCode, "SubProductRoutes");
+  const moduleClass = await loadFactory(moduleCode, SUB_PRODUCT_ROUTES);
   if (!moduleClass) {
     throw new Error(
       `${module} Module :Module code ${moduleCode} class not found`
