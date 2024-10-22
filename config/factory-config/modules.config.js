53f50b9fc00665fc1eb12b308532ad89a57ecfb2
suryameiyappan <137778852+suryameiyappan@users.noreply.github.com>
2024-10-22 12:37:15 +0530

    Update modules.config.js

--- config/factory-config/modules.config.js
+++ config/factory-config/modules.config.js
@@ -3,12 +3,12 @@ const
   redisClient = require("../db-config/redis.database").connectRedisClient(),
   productConfigRepository = require("../../repositories/product-config.repositories");
 
-exports.getProduct = async (product) => {
-  let key = `product_routes:${product}`;
+exports.loadFactory = async (module, path) => {
+  let key = `${path}:${module}`;
   const cachedRoute = await redisClient.getKey(key);
 
   if (!cachedRoute) {
-    const route = await productConfigRepository.productRoutes(product);
+    const route = await productConfigRepository.productConfig(module, path);
     await redisClient.setKey(key, JSON.stringify(route), 'EX', 172800);
   }
   
