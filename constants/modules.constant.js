ade10b18a39e998259798626c80896447af8ea22
suryameiyappan <137778852+suryameiyappan@users.noreply.github.com>
2024-10-22 13:07:54 +0530

    Create modules.constant.js

---
+++ constants/modules.constant.js
@@ -0,0 +1,9 @@
+const PRODUCT_ROUTES = "ProductRoutes";
+const VALIDATOR_CONFIG = "ValidatorConfig";
+const SUB_PRODUCT_ROUTES = "SubProductRoutes";
+
+module.exports = {
+    PRODUCT_ROUTES,
+    VALIDATOR_CONFIG,
+    SUB_PRODUCT_ROUTES
+};
